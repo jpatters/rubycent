6a6f2a20ee53d868879a1c83d92dbc72658dad0d
Jordan Patterson <jordan@jpatterson.me>
2020-06-25 14:21:40 -0300

    added optional path prefix

--- lib/centrifuge/client.rb
+++ lib/centrifuge/client.rb
@@ -1,24 +1,25 @@
-require 'multi_json'
-require 'httpclient'
-require 'centrifuge/builder'
+require "multi_json"
+require "httpclient"
+require "centrifuge/builder"
 
 module Centrifuge
   class Client
     DEFAULT_OPTIONS = {
-      scheme: 'http',
-      host: 'localhost',
+      scheme: "http",
+      host: "localhost",
       port: 8000,
+      path_prefix: ""
     }
-    attr_accessor :scheme, :host, :port, :secret
+    attr_accessor :scheme, :host, :port, :secret, :path_prefix
     attr_writer :connect_timeout, :send_timeout, :receive_timeout,
-    :keep_alive_timeout
+      :keep_alive_timeout
 
     ## CONFIGURATION ##
 
     def initialize(options = {})
       options = DEFAULT_OPTIONS.merge(options)
-      @scheme, @host, @port, @secret = options.values_at(
-       :scheme, :host, :port, :secret
+      @scheme, @host, @port, @secret, @path_prefix = options.values_at(
+        :scheme, :host, :port, :secret, :path_prefix
       )
       set_default_timeouts
     end
@@ -35,53 +36,53 @@ module Centrifuge
         scheme: scheme.to_s,
         host: host,
         port: port,
-        path: "/api/#{path}"
+        path: File.join(path_prefix, "/api/", path)
       })
     end
 
     def broadcast(channels, data)
-      Centrifuge::Builder.new('broadcast', { channels: channels, data: data }, self).process
+      Centrifuge::Builder.new("broadcast", {channels: channels, data: data}, self).process
     end
 
     def publish(channel, data)
-      Centrifuge::Builder.new('publish', { channel: channel, data: data }, self).process
+      Centrifuge::Builder.new("publish", {channel: channel, data: data}, self).process
     end
 
     def unsubscribe(channel, user)
-      Centrifuge::Builder.new('unsubscribe', { channel: channel, user: user }, self).process
+      Centrifuge::Builder.new("unsubscribe", {channel: channel, user: user}, self).process
     end
 
     def disconnect(user)
-      Centrifuge::Builder.new('disconnect', { user: user }, self).process
+      Centrifuge::Builder.new("disconnect", {user: user}, self).process
     end
 
     def presence(channel)
-      Centrifuge::Builder.new('presence', { channel: channel }, self).process
+      Centrifuge::Builder.new("presence", {channel: channel}, self).process
     end
 
     def history(channel)
-      Centrifuge::Builder.new('history', { channel: channel }, self).process
+      Centrifuge::Builder.new("history", {channel: channel}, self).process
     end
 
-    def channels()
-      Centrifuge::Builder.new('channels', {}, self).process
+    def channels
+      Centrifuge::Builder.new("channels", {}, self).process
     end
 
-    def stats()
-      Centrifuge::Builder.new('stats', {}, self).process
+    def stats
+      Centrifuge::Builder.new("stats", {}, self).process
     end
 
     def token_for(user, timestamp, user_info = "")
       sign("#{user}#{timestamp}#{user_info}")
     end
 
-    def generate_channel_sign(client, channel, user_info="")
+    def generate_channel_sign(client, channel, user_info = "")
       data = "#{client}#{channel}#{user_info}"
-      OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, secret, data)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("SHA256"), secret, data)
     end
 
     def sign(body)
-      OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, secret, body)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("SHA256"), secret, body)
     end
 
     def client
@@ -94,6 +95,5 @@ module Centrifuge
         end
       end
     end
-
   end
 end
